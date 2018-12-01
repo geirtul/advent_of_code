7181c48a4390ebc92aaafedd260cb59b3c75eee4
Geir Tore Ulvik <ulvik@luring.uio.no>
2018-12-01 15:48:08 +0100

    Finds solution, but slowly.

--- day01/frequency.pl
+++ day01/frequency.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 
+use List::Util "any";
 
 my $file = "input.txt";
 # Open file and read contents into array
@@ -9,8 +10,43 @@ open my $input, '<', $file or die "can't open $file: $!";
 chomp(my @lines = <$input>);
 close $input or die "can't close $file: $!";
 
-my $sum = 0;
-foreach my $line (@lines){
-    $sum += $line;
+
+# =========== Solve the puzzle ============
+my @test = (-6, +3, +8, +5, -6);
+my $part1 = find_frequency_sum(@lines);
+print "Part 1: $part1\n";
+my $part2 = get_repeated_frequency(@lines);
+print "Part 2: $part2\n";
+
+# =========== Subroutine declarations ============
+sub find_frequency_sum {
+  my @frequencies = @_;
+
+  my $sum = 0;
+  foreach my $freq (@frequencies){
+    $sum += $freq;
+  }
+  return $sum
 }
-print $sum."\n";
+
+sub get_repeated_frequency {
+  my @frequencies = @_;
+
+  my @freqs = (0);
+  my $num_list_runs = 0;
+  while (1) {
+    # Loop through list
+    foreach my $freq (@frequencies){
+      my $new_freq = $freqs[-1] + $freq;
+      # Check if the new frequency is in the frequency list, return if it is.
+      my %hash = map {$_, 1} @freqs;
+      if ($hash{ $new_freq }) {
+        return $new_freq;
+      }
+      # Append the new frequency to the list of known freqs
+      push @freqs, $new_freq;
+    }
+    $num_list_runs += 1;
+    print "List runs: $num_list_runs\n";
+  }
+}# END subroutine get_repeated_frequency
