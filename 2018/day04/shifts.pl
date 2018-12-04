a9337e3e25463c15cfe83b138625261837aef8f1
Geir Tore Ulvik <ulvik@usit.uio.no>
2018-12-04 15:32:57 +0100

    Made testset. Working on calculating sleeptime.

--- 2018/day04/shifts.pl
+++ 2018/day04/shifts.pl
@@ -6,35 +6,28 @@ use DateTime;
 use Data::Dumper;
 use List::Util ("none", "any");
 
-my $file = "input.txt";
-#open my $input, '<', $file or die "can't open $file: $!";
-#chomp(my @lines = <$input>);
-#close $input or die "can't close $file: $!";
+my $file = "test.txt";
+open my $input, '<', $file or die "can't open $file: $!";
+chomp(my @lines = <$input>);
+close $input or die "can't close $file: $!";
 
-my @test = qw(
-  [1518-11-01 00:00] Guard #10 begins shift,
-  [1518-11-01 00:05] falls asleep,
-  [1518-11-01 00:25] wakes up,
-  [1518-11-01 00:30] falls asleep,
-  [1518-11-01 00:55] wakes up,
-  [1518-11-01 23:58] Guard #99 begins shift,
-  [1518-11-02 00:40] falls asleep,
-  [1518-11-02 00:50] wakes up,
-  [1518-11-03 00:05] Guard #10 begins shift,
-  [1518-11-03 00:24] falls asleep,
-  [1518-11-03 00:29] wakes up,
-  [1518-11-04 00:02] Guard #99 begins shift,
-  [1518-11-04 00:36] falls asleep,
-  [1518-11-04 00:46] wakes up,
-  [1518-11-05 00:03] Guard #99 begins shift,
-  [1518-11-05 00:45] falls asleep,
-  [1518-11-05 00:55] wakes up
-)
 
 # =========== Solve the puzzle ============
 # Part 1:
-
+@lines = sort(@lines);
+calc_sleeptime(@lines);
 # Part 2:
 
 
 # =========== Subroutine declarations ============
+
+sub calc_sleeptime {
+  my @in = @_;
+  # Match datetime, what happens, and guard ID.
+  my $regex = qr/\[(.+)\]\s(Guard|falls|wakes)\s(#\d+|asleep|up)/;
+  foreach $_ (@in) {
+    my @match = $_ =~ $regex;
+    print Dumper(\@match);
+  }
+
+}
