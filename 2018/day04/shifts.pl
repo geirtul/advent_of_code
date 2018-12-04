a584a1b809280b7d0565bed397b0fded4f5793a5
Geir Tore Ulvik <ulvik@usit.uio.no>
2018-12-04 16:02:06 +0100

    Algo decided. Working on implementation.

--- 2018/day04/shifts.pl
+++ 2018/day04/shifts.pl
@@ -21,13 +21,25 @@ calc_sleeptime(@lines);
 
 # =========== Subroutine declarations ============
 
-sub calc_sleeptime {
+sub calc_sleeptimes {
+  # Create a hash with Guard IDs as keys, and sleeping start/stop as values
+  # by storing a reference to the array containing it.
   my @in = @_;
   # Match datetime, what happens, and guard ID.
   my $regex = qr/\[(.+)\]\s(Guard|falls|wakes)\s(#\d+|asleep|up)/;
+  my $current_id;
+  my %sleeptimes;
   foreach $_ (@in) {
     my @match = $_ =~ $regex;
-    print Dumper(\@match);
+    if ($match[1] eq "Guard") {
+      $current_id = $match[1];
+      if (exists($sleeptimes{$current_id}) {
+        
+      }
+      next; # Just need the ID, move on to data.
+    }
+    # Get start or stop time for each event and store in array which ref is
+    # in the hash
   }
 
 }
