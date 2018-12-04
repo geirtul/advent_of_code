d70604e938cf1b43cce07a1662e2c64dde47a19f
Geir Tore Ulvik <ulvik@luring.uio.no>
2018-12-04 17:36:28 +0100

    sleeptimes hash works nicely.

--- 2018/day04/shifts.pl
+++ 2018/day04/shifts.pl
@@ -15,31 +15,56 @@ close $input or die "can't close $file: $!";
 # =========== Solve the puzzle ============
 # Part 1:
 @lines = sort(@lines);
-calc_sleeptime(@lines);
+my $sleeps = calc_sleeptimes(@lines);
+print Dumper($sleeps);
 # Part 2:
 
 
 # =========== Subroutine declarations ============
+
+sub total_sleep {
+  # Calculate the total amount of sleep for an agent
+  # based on sleep/wake times in sleeptimes hash.
+  my $arr_ref = shift;
+  my %sleeptimes = %$arr_ref;
+  
+}
 
 sub calc_sleeptimes {
   # Create a hash with Guard IDs as keys, and sleeping start/stop as values
   # by storing a reference to the array containing it.
   my @in = @_;
   # Match datetime, what happens, and guard ID.
-  my $regex = qr/\[(.+)\]\s(Guard|falls|wakes)\s(#\d+|asleep|up)/;
+  my $regex = qr/\[.+:(\d\d)\]\s(Guard|falls|wakes)\s(#\d+|asleep|up)/;
   my $current_id;
   my %sleeptimes;
   foreach $_ (@in) {
     my @match = $_ =~ $regex;
+    my $time = $match[0];
+    # If it's a guard event, set current id.
     if ($match[1] eq "Guard") {
-      $current_id = $match[1];
-      if (exists($sleeptimes{$current_id}) {
-
-      }
+      $current_id = $match[2];
       next; # Just need the ID, move on to data.
     }
-    # Get start or stop time for each event and store in array which ref is
-    # in the hash
+    # if it's a sleep-even, append sleeptime.
+    if ($match[1] eq "falls"){
+      # Check if id exists in hash
+      if (exists($sleeptimes{$current_id})) {
+        push(@{$sleeptimes{$current_id}}, $time);
+      } else {
+        my @arr = $time;
+        $sleeptimes{$current_id} = \@arr;
+      }
+    }
+    # If it's a wakes-event, append waketime
+    if ($match[1] eq "wakes"){
+      if (exists($sleeptimes{$current_id})) {
+        push(@{$sleeptimes{$current_id}}, $time);
+      } else {
+        my @arr = $time;
+        $sleeptimes{$current_id} = \@arr;
+      }
+    }
   }
-
+  return \%sleeptimes;
 }
