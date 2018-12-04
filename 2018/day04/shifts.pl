c08810358418bfcfdc00a3ab626cdecc28cde170
Geir Tore Ulvik <ulvik@luring.uio.no>
2018-12-04 20:32:54 +0100

    Day 4 complete.

--- 2018/day04/shifts.pl
+++ 2018/day04/shifts.pl
@@ -6,7 +6,7 @@ use DateTime;
 use Data::Dumper;
 use List::UtilsBy qw(max_by);
 
-my $file = "test.txt";
+my $file = "input.txt";
 open my $input, '<', $file or die "can't open $file: $!";
 chomp(my @lines = <$input>);
 close $input or die "can't close $file: $!";
@@ -18,21 +18,58 @@ close $input or die "can't close $file: $!";
 my $sleeps = calc_sleeptimes(@lines);
 my $totals = total_sleep($sleeps);
 my $sleepy_guard = max_by {%$totals{$_}} keys(%$totals);
-print "The guard sleeping the most is: $sleepy_guard";
-# Part 2:
+my ($most_frequent, $freq) = &most_frequent_sleeptime($sleeps, $sleepy_guard);
+my $result = $sleepy_guard*$most_frequent;
+print "The guard sleeping the most is: $sleepy_guard\n";
+print "The minute he sleeps the most is: $most_frequent\n";
+print "The result is: $result with $freq times.";
 
+# Part 2:
+my $steady_guard;
+my $highest_freq = 0;
+my $steady_minute;
+foreach my $guard (keys(%$sleeps)) {
+  my ($minute, $freq) = &most_frequent_sleeptime($sleeps, $guard);
+  if ($freq > $highest_freq) {
+    $steady_minute = $minute;
+    $highest_freq = $freq;
+    $steady_guard = $guard;
+  }
+}
+my $result2 = $steady_guard*$steady_minute;
+print "The guard sleeping on a minute the most is: $steady_guard\n";
+print "The minute and number of times are: $steady_minute , $highest_freq\n";
+print "The result is: $result2\n";
 
 # =========== Subroutine declarations ============
+sub most_frequent_sleeptime {
+  # Based on sleep/wake time of the most sleepy guard,
+  # find the minute(s) the guard is most often asleep.
+  my $aref = shift;
+  my $guard = shift;
+
+  my %frequencies;
+  my @times = @{$aref->{$guard}};
+  my $i = 0;
+  while ($i < scalar(@times)) {
+      foreach $_ ($times[$i]..($times[$i+1]-1)) {
+        $frequencies{$_}++;
+      }
+      $i += 2;
+  }
+  my $most_frequent = max_by {$frequencies{$_}} keys(%frequencies);
+  my $freq = $frequencies{$most_frequent};
+  return ($most_frequent, $freq);
+}
 
 sub total_sleep {
   # Calculate the total amount of sleep for an agent
   # based on sleep/wake times in sleeptimes hash.
-  my $aref = shift; # array reference
+  my $href = shift; # hash reference
   #print Dumper($aref);
   my %total_times;
-  foreach my $key (keys(%$aref)) {
-    print "Key is $key\n";
-    my @times = @{$aref->{$key}};
+  foreach my $key (keys(%$href)) {
+    my @times = @{$href->{$key}};
     my $sum = 0;
     my $i = 0;
     # Know times come in pairs of sleep, wake.
@@ -51,7 +88,7 @@ sub calc_sleeptimes {
   # by storing a reference to the array containing it.
   my @in = @_;
   # Match datetime, what happens, and guard ID.
-  my $regex = qr/\[.+:(\d\d)\]\s(Guard|falls|wakes)\s(#\d+|asleep|up)/;
+  my $regex = qr/\[.+:(\d\d)\]\s(Guard|falls|wakes)\s#?(\d+|asleep|up)/;
   my $current_id;
   my %sleeptimes;
   foreach $_ (@in) {
