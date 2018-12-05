e857d19e5ad4abd1cf8542e9386c6688f5f048e8
Geir Tore Ulvik <ulvik@luring.uio.no>
2018-12-05 14:55:15 +0100

    Added template for day 6

---
+++ 2018/day06/tmp.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Data::Dumper;
+use List::Util qw(max min);
+use List::UtilsBy qw(max_by);
+
+# File import
+my $file = "input.txt";
+open my $input, '<', $file or die "can't open $file: $!";
+chomp(my $line = <$input>);
+close $input or die "can't close $file: $!";
+
+# Preparation
+
+
+# Solve the puzzle
+## Part 1:
+
+
+## Part 2:
+
+# Subroutine declarations
