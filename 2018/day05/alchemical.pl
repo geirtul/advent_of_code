f850f0fdb1e7c221e101b9b23a907a6d43f65151
Geir Tore Ulvik <ulvik@luring.uio.no>
2018-12-05 12:59:45 +0100

    Fixed implementation. Much faster now.

--- 2018/day05/alchemical.pl
+++ 2018/day05/alchemical.pl
@@ -6,7 +6,7 @@ use DateTime;
 use Data::Dumper;
 use List::UtilsBy qw(max_by);
 
-my $file = "test.txt";
+my $file = "input.txt";
 open my $input, '<', $file or die "can't open $file: $!";
 chomp(my $line = <$input>);
 close $input or die "can't close $file: $!";
@@ -21,8 +21,8 @@ foreach $_ ("A".."Z"){
 }
 # =========== Solve the puzzle ============
 # Part 1:
-react(\@polymer, \%charmap);
-print scalar (@polymer);
+my @reacted = react(\@polymer, \%charmap);
+print scalar (@reacted);
 
 # Part 2:
 
@@ -32,18 +32,19 @@ sub react {
   # React a string split into an array.
   my $aref = shift; # Reference to array
   my $href = shift; # Reference to charmap hash
-  #my @polymer = @$aref;
-  #my %charmap = %$href;
-  my $cond = "true";
-  OUTER: while ($cond eq "true"){
-    for (my $i=0; $i < (scalar(@$aref)-1); $i++) {
-      TEST: if (abs($href->{@$aref[$i]} - $href->{@$aref[$i+1]}) == 32) {
-        # Remove array elements and go to next while loop if i is high
-        splice(@$aref, $i, 2);
-        $i--;
-        goto TEST;
-      }
+  my @polymer = @$aref;
+  my $polymer_length = scalar(@polymer);
+  my $i = 0;
+  while ($i < $polymer_length-1){
+    if (abs($href->{$polymer[$i]} - $href->{$polymer[$i+1]}) == 32) {
+      # Remove array elements and go to next while loop if i is high
+      splice(@polymer, $i, 2);
+      $polymer_length -= 2;
+      next if ($i == 0);
+      $i--;
+      next;
     }
-    $cond = "false";
+    $i++;
   }
+  return @polymer
 }
