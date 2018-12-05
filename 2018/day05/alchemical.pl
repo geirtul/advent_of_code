fdfc08e9f8ea1dca50bb9ec03a6820965bc2e6bf
Geir Tore Ulvik <ulvik@luring.uio.no>
2018-12-05 14:52:18 +0100

    Code runs in ~0.3s. Fairly ok.

--- 2018/day05/alchemical.pl
+++ 2018/day05/alchemical.pl
@@ -2,49 +2,62 @@
 use strict;
 use warnings;
 
-use DateTime;
 use Data::Dumper;
+use List::Util qw(max min);
 use List::UtilsBy qw(max_by);
 
+# =========== File import ============
 my $file = "input.txt";
 open my $input, '<', $file or die "can't open $file: $!";
 chomp(my $line = <$input>);
 close $input or die "can't close $file: $!";
-my @polymer = split(//, $line);
 
+# =========== Preparation ============
+# Set up a hash that maps characters to their numeric value
+# to avoid calculating it every time in every loop later.
 my %charmap;
-foreach $_ ("a".."z"){
+my @lowercase = ("a".."z");
+my @uppercase = ("A".."Z");
+foreach $_ (@lowercase){
   $charmap{$_} = ord($_);
 }
-foreach $_ ("A".."Z"){
+foreach $_ (@uppercase){
   $charmap{$_} = ord($_);
 }
+
 # =========== Solve the puzzle ============
 # Part 1:
-my @reacted = react(\@polymer, \%charmap);
-print scalar (@reacted);
+my $reacted = react($line, \%charmap);
+print "Part 1: ".length($reacted)."\n";
 
 # Part 2:
+my @sizes; # Store sizes of reacted strings
+for (my $i=0; $i < scalar(@lowercase); $i++) {
+  my $l = $lowercase[$i];
+  my $u = $uppercase[$i];
+  my $tmp = $reacted =~ s/[$l$u]//gr;
+  push(@sizes, length(react($tmp, \%charmap)));
+}
+print "Part 2: ".min(@sizes)."\n";
 
 # =========== Subroutine declarations ============
 
 sub react {
-  # React a string split into an array.
-  my $aref = shift; # Reference to array
+  # React a string
+  my $string = shift;
   my $href = shift; # Reference to charmap hash
-  my @polymer = @$aref;
-  my $polymer_length = scalar(@polymer);
+  my @polymer = split(//, $string); # Split string to array
+  my $polymer_length = scalar(@polymer); # keep track of length
   my $i = 0;
   while ($i < $polymer_length-1){
     if (abs($href->{$polymer[$i]} - $href->{$polymer[$i+1]}) == 32) {
-      # Remove array elements and go to next while loop if i is high
-      splice(@polymer, $i, 2);
-      $polymer_length -= 2;
-      next if ($i == 0);
-      $i--;
+      splice(@polymer, $i, 2); # Remove the two elements
+      $polymer_length -= 2; # Update current polymer length
+      next if ($i == 0); # If first element was removed, stay on index 0.
+      $i--; #Jump back one index and go to 'next' while loop.
       next;
     }
     $i++;
   }
-  return @polymer
+  return join("", @polymer);
 }
