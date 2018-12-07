7715407a9f95034c06491cb0056a728df2ca2f8f
Geir Tore Ulvik <ulvik@luring.uio.no>
2018-12-07 15:06:41 +0100

    Adding base files for day 7.

---
+++ 2018/day07/new.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Data::Dumper;
+use List::Util qw(max min any);
+use List::UtilsBy qw(max_by);
+
+# File import
+my $file = "test.txt";
+open my $input, '<', $file or die "can't open $file: $!";
+chomp(my @lines = <$input>);
+close $input or die "can't close $file: $!";
+
+# Preparation
+
+# Solve the puzzle
+## Part 1:
+
+
+## Part 2:
+
+# Subroutine declarations
