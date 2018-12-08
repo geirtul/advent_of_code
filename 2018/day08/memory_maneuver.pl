61891bad20af5700efbff9b0ff9d63157fa2ade2
Geir Tore Ulvik <ulvik@luring.uio.no>
2018-12-09 00:36:59 +0100

    Implementing recursive treebuilding.

--- 2018/day08/memory_maneuver.pl
+++ 2018/day08/memory_maneuver.pl
@@ -20,10 +20,33 @@ print @license_file;
 
 
 # Solve the puzzle
-## Part 1:
+## Part 1: 2 3 [0 3 10 11 12] 1 1 [0 1 99] 2 1 1 2
 print "\nPart 1:\n";
+my %tree;
 
 ## Part 2:
 print "\nPart 2:\n";
 
 # Subroutine declarations
+sub build_node {
+  # Assumes node can't have no metadata
+  my $index = shift;
+  my $n_children = $licence_file[$index];
+  my $n_metadata = $licence_file[$index+1];
+  if ($n_children == 0) {
+    my @metadata;
+    my $meta_index = $index + 2;
+    # Bytt ut while loop med splice
+    while ($meta_index < $index + 1 + $n_metadata) {
+      push(@metadata, $licence_file[$meta_index]);
+      $meta_index++;
+    }
+    my $name = scalar(keys(%tree))+1;
+    $tree{$name} = \@metadata; #Set ref to current metadata.
+    return;
+  }
+  # If the number of children is larger than one though, we have work to do.
+
+
+
+}
