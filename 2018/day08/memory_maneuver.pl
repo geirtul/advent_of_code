d0e7374ced313c78a2514bb4c286e76403f32f13
Geir Tore Ulvik <ulvik@luring.uio.no>
2018-12-10 13:21:00 +0100

    Working on node references.

--- 2018/day08/memory_maneuver.pl
+++ 2018/day08/memory_maneuver.pl
@@ -16,40 +16,55 @@ close $input or die "can't close $file: $!";
 
 # Preparation
 my @license_file = split(/\s/, $lines[0]);
-print @license_file;
 
 
 # Solve the puzzle
-## Part 1: 2 3 [0 3 10 11 12] 1 1 [0 1 99] 2 1 1 2
+## Part 1:
 print "\nPart 1:\n";
 my %tree;
 build_node(0);
+print Dumper(\%tree);
+my $sum_metadata = sum_metadata(\%tree);
+print "Sum of metadata = $sum_metadata\n";
 ## Part 2:
 print "\nPart 2:\n";
 
 # Subroutine declarations
+sub sum_metadata {
+  # Sum all the metadata in the tree.
+  my $hashref = shift;
+  my $metasum = 0;
+  foreach my $k (keys %$hashref) {
+    my @arr = @{$hashref->{$k}};
+    my $tmp_sum = 0;
+    for (my $i=2; $i < scalar(@arr); $i++) {
+      $tmp_sum += $arr[$i];
+    }
+    $metasum += $tmp_sum;
+  }
+  return $metasum;
+}
+
 sub build_node {
-  # Assumes node can't have no metadata
   my $index = shift;
   my $n_children = $license_file[$index];
   my $n_metadata = $license_file[$index+1];
   my $iter = 0;
-  my $meta_index = $index + 2;
   if ($n_children == 0) {
-    my @metadata = splice(@license_file, $index, $n_metadata+2);
-    print "@metadata";
-    print "\n";
-    my $name = scalar(keys(%tree))+1;
-    $tree{$name} = \@metadata; #Set ref to current metadata.
+    my @data = splice(@license_file, $index, $n_metadata+2);
+    my $name = scalar(keys(%tree));
+    $tree{$name} = \@data; #Set ref to current metadata.
     return;
   }
   for (my $i=0; $i < $n_children; $i++) {
     build_node($index + 2);
   }
-  my @metadata = splice(@license_file, $index, $n_metadata+2);
-  print "@metadata";
-  print "\n";
-  my $name = scalar(keys(%tree)) + 1;
-  $tree{$name} = \@metadata; #Set ref to current metadata.
+  my @data = splice(@license_file, $index, $n_metadata+2);
+  if ($index == 0) {
+    $name = 'root';
+  } else {
+    my $name = scalar(keys(%tree));
+  }
+  $tree{$name} = \@data; #Set ref to current metadata.
   return;
 }
