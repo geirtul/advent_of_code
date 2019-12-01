37854eb9230c6cdf57a153d76db3ef8c10f108d7
Geir Tore Ulvik <ulvik@luring.uio.no>
2019-12-01 16:12:11 +0100

    Day 01 complete.

--- 2019/day01/day01.kt
+++ 2019/day01/day01.kt
@@ -2,23 +2,30 @@ import java.io.File
 
 // Day one of advent of code 2019
 
+fun fuel(mass: Int) {
+    return mass/3 - 2    
+}
+
+fun fuelMass(mass: Int) {
+    var fuel_total: Int = 0
+    var curr_fuel = fuel(mass)
+    while(curr_fuel > 0) {
+        fuel_total += curr_fuel
+        curr_fuel = fuel(curr_fuel)
+    }
+    return fuel_total
+}
+
 fun main(args: Array<String>) {
     val file = File("input.txt")
     var mass_list:List<String> = file.readLines()
-    var fuel_needed: Int = 0
+
+    var fuel_mass: Int = 0
     var fuel_total: Int = 0
     for(mass in mass_list){
-        var fuel: Int = mass.toInt()/3 - 2
-        fuel_needed += fuel
-        fuel_total += fuel
+        fuel_mass += fuel(mass)
+        fuel_total += fuelMass(mass)
 
-        // Part 2
-        while(fuel > 0) {
-            fuel = fuel/3 - 2
-            if (fuel > 0) {
-                fuel_total += fuel
-            }
-        }
     }
     println("Fuel part 1: $fuel_needed")
     println("Fuel part 2: $fuel_total")
