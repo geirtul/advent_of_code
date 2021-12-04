536a73761d9a67f2ea2655b6ca00a21f0ff87c3b
Geir Ulvik <gt.ulvik@gmail.com>
2021-12-05 00:30:44 +0100

    Update bingoclass and add bingoboard class and also add test input

--- 2021/Bingo.java
+++ 2021/Bingo.java
@@ -1,7 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.*;
+
 public class Bingo {
     int[] bingoNumbers;
 
     public void loadGame(String filename) {
+        /**
+         * Read in all information about the bingo game. Line 1 are
+         * numbers drawn. The rest of the lines contain bingoboards.
+         */
 
+        ArrayList<String> bingoInfo = Reader.readLines(filename);
+
+        // Get numbers drawn
+        this.bingoNumbers = Arrays.stream(
+            bingoInfo.get(0).split(",")
+        ).mapToInt(Integer::parseInt).toArray();
+        
     }
 }
---
+++ 2021/Day04.java
@@ -0,0 +1,19 @@
+public class Day04 {
+
+    public static void solveOne(String filename) {
+        Bingo bingo = new Bingo();
+        bingo.loadGame(filename);
+        // Submarine sub = new Submarine();
+        // System.out.format("Part 1: %d\n", sub.diagnostic.powerConsumption);
+    }
+
+    public static void solveTwo(String filename) {
+        // Submarine sub = new Submarine();
+        // System.out.format("Part 2: %d\n", );
+    }
+
+    public static void main(String[] args) {
+        solveOne(args[0]);
+        // solveTwo(args[0]);
+    }
+}
