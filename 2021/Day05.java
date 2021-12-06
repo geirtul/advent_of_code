ba637356a6d4a35a8953d39ca77509afac103085
Geir Ulvik <gt.ulvik@gmail.com>
2021-12-06 11:29:29 +0100

    Adding initial files for day 5 + input day 4 and test_input day5

---
+++ 2021/Day05.java
@@ -0,0 +1,16 @@
+public class Day05 {
+
+    public static void solveOne(String filename) {
+        Submarine sub = new Submarine();
+        //System.out.format("Part 1: %s\n", );
+    }
+
+    public static void solveTwo(String filename) {
+        Submarine sub = new Submarine();
+        //System.out.format("Part 2: %s\n", );
+    }
+    public static void main(String[] args) {
+        solveOne(args[0]);
+        // solveTwo(args[0]);
+    }
+}
