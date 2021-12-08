18d61db13c4374d22d3975ca8a740c1a65a199ac
Geir Ulvik <gt.ulvik@gmail.com>
2021-12-08 10:15:32 +0100

    Answer with avg too high

--- 2021/Day07.java
+++ 2021/Day07.java
@@ -13,16 +13,44 @@ public class Day07 {
         }
         positions.sort(Comparator.naturalOrder());
         int median = positions.get(positions.size()/2);
-        System.out.println(median);
 
-        //System.out.format("Part 1: %s\n", );
+        int requiredFuel = 0;
+        for (Integer pos : positions) {
+            requiredFuel += Math.abs(pos - median);
+        }
+        
+        System.out.format("Part 1: %d\n", requiredFuel);
     }
 
     public static void solveTwo(String filename) {
-        //System.out.format("Part 2: %s\n", );
+        String input = Reader.readLine(filename);
+        ArrayList<Integer> positions = new ArrayList<Integer>();
+        for (String position : input.split(",")) {
+            positions.add(Integer.parseInt(position));
+        }
+        positions.sort(Comparator.naturalOrder());
+        int avg;
+        int tmp = 0;
+        for (Integer pos : positions) {
+            tmp += pos;
+        }
+        avg = (int)Math.ceil((double)tmp/positions.size());
+
+        int requiredFuel = 0;
+        for (Integer pos : positions) {
+            int diff = Math.abs(pos - avg);
+            int fuel = 0;
+            for (int i = 1; i <= diff; i++) {
+                fuel+=i;                
+            }
+            requiredFuel += fuel;
+        }
+        
+        System.out.format("Part 2: %d\n", requiredFuel);
     }
     public static void main(String[] args) {
-        solveOne(args[0]);
-        // solveTwo(args[0]);
+        // solveOne(args[0]);
+        solveTwo(args[0]);
+        // 94813677 too high
     }
 }
