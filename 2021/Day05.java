ccf93db08d36b0005f4b5617968880b3ef59f2c8
Geir Ulvik <gt.ulvik@gmail.com>
2021-12-08 08:21:36 +0100

    Update Line

--- 2021/Day05.java
+++ 2021/Day05.java
@@ -9,21 +9,24 @@ public class Day05 {
         int xMax = 0;
         int yMax = 0;
         for (int[] coords : test) {
-            int[] start = new int[]{coords[0], coords[1]};
-            int[] end = new int[]{coords[2], coords[3]};
+            int x1, y1, x2, y2;
+            x1 = coords[0];
+            y1 = coords[1];
+            x2 = coords[2];
+            y2 = coords[3];
             
             // Skip lines that are not horizontal or vertical
-            if (start[0] != end[0] && start[1] != end[1]) {
+            if (x1 != x2 && y1 != y2) {
                 continue;
             }
 
             // Update xmax and ymax
-            xMax = Math.max(Math.max(start[0], end[0]), xMax);
-            yMax = Math.max(Math.max(start[1], end[1]), yMax);
+            xMax = Math.max(Math.max(x1, x2), xMax);
+            yMax = Math.max(Math.max(y1, y2), yMax);
             
-            lines.add(new Line2D(start, end));
+            lines.add(new Line2D(coords));
         }
-        // Set up array to keep track of points where all lines
+        // Set up array to keep track of points of all lines
         int[][] grid = new int[xMax + 1][yMax + 1];
         for (int i = 0; i < lines.size(); i++) {
             Line2D currentLine = lines.get(i);
@@ -49,51 +52,12 @@ public class Day05 {
     }
 
     public static void solveTwo(String filename) {
-        ArrayList<int[]> test = Reader.readDay5(filename, "(\\d+),(\\d+) -> (\\d+),(\\d+)");
-        ArrayList<Line2D> lines = new ArrayList<Line2D>();
-        int xMax = 0;
-        int yMax = 0;
-        for (int[] coords : test) {
-            int[] start = new int[]{coords[0], coords[1]};
-            int[] end = new int[]{coords[2], coords[3]};
-            
-            // Update xmax and ymax
-            xMax = Math.max(Math.max(start[0], end[0]), xMax);
-            yMax = Math.max(Math.max(start[1], end[1]), yMax);
-            
-            lines.add(new Line2D(start, end));
-        }
-        // Set up array to keep track of points where all lines
-        int[][] grid = new int[xMax + 1][yMax + 1];
-        for (int i = 0; i < lines.size(); i++) {
-            Line2D currentLine = lines.get(i);
-            for (int j = i + 1; j < lines.size(); j++) {
-                Line2D otherLine = lines.get(j);
-                for (Integer idx : currentLine.overlappingPoints(otherLine)) {
-                    int[] point = currentLine.points[idx];
-                    grid[point[0]][point[1]]++;
-                }
-            }
-        }
-
-        int numOverlapping = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] > 0) {
-                    numOverlapping++;
-                }
-            }
-            
-        }
-        for (Line2D meh : lines) {
-            meh.printLine();
-        }
-        System.out.format("Part 2: %d\n", numOverlapping);
+        // System.out.format("Part 2: %d\n", numOverlapping);
         // 7196 too low
     }
     public static void main(String[] args) {
-        // solveOne(args[0]);
-        solveTwo(args[0]);
+        solveOne(args[0]);
+        // solveTwo(args[0]);
     }
 }
 
--- 2021/Line2D.java
+++ 2021/Line2D.java
@@ -4,43 +4,60 @@ import java.util.Arrays;
 public class Line2D {
     int[][] points;
 
-    public Line2D(int[] start, int[] end){
-        this.points = generatePoints(start, end);
+    public Line2D(int[] points){
+        this.points = generatePoints(points);
     }
 
 
-    public int[][] generatePoints(int[] start, int[] end) {
+    public int[][] generatePoints(int[] points) {
         /**
          * Generate the points on the line between <start> and <end>
          */
 
         int x1, y1, x2, y2;
-        x1 = start[0];
-        y1 = start[1];
-        x2 = end[0];
-        y2 = end[1];
-        int sizeX = Math.abs(x2 - x1);
-        int sizeY = Math.abs(y2 - y1);
-        int[][] points = new int[Math.max(sizeX, sizeY) + 1][2];
-        int idxStart = 0;
-        if (sizeX < sizeY){
-            idxStart = 1;
+        x1 = points[0];
+        y1 = points[1];
+        x2 = points[2];
+        y2 = points[3];
+
+        // Determine line direction
+        String dir;
+        int p1, p2;
+        int diff = 0;
+        if (x1 != x2 && y1 == y2) {
+            dir = "horizontal";
+            p1 = Math.min(x1, x2);
+            p2 = Math.max(x1, x2);
+        } else if (x1 == x2 && y1 != y2) {
+            dir = "vertical";
+            p1 = Math.min(y1, y2);
+            p2 = Math.max(y1, y2);
+        } else {
+            dir = "diagonal";
+            p1 = Math.min(x1, x2);
+            p2 = Math.max(x1, x2);
+            diff = y1 - x1;
         }
-        int p1 = Math.min(start[idxStart], end[idxStart]);
-        int p2 = Math.max(start[idxStart], end[idxStart]);
+        int[][] linePoints = new int[p2 - p1 + 1][2];
         int pointsIdx = 0;
         for (int i = p1; i <= p2; i++) {
-            if (sizeX > sizeY) {
-                points[pointsIdx] = new int[]{i, end[1]};
-            } else if (sizeY > sizeX){
-                points[pointsIdx] = new int[]{end[0], i};
+            if (dir.equals("horizontal")) {
+                linePoints[pointsIdx] = new int[]{i, y1};
+            } else if (dir.equals("vertical")) {
+                linePoints[pointsIdx] = new int[]{x1, i};
             } else {
-                points[pointsIdx] = new int[]{i, i};
+                if (y1 < x1) {
+                    linePoints[pointsIdx] = new int[]{i, p2-pointsIdx};
+                } else if (x1 < y1) {
+                    linePoints[pointsIdx] = new int[]{i, i + diff};
+                } else {
+                    linePoints[pointsIdx] = new int[]{i, i};
+                }
             }
             pointsIdx++;
         }
 
-        return points;
+        return linePoints;
     }
 
     public ArrayList<Integer> overlappingPoints(Line2D other) {
