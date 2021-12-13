2e189004b4f01ec66f4157555fb5dea28fb52aef
Geir Ulvik <gt.ulvik@gmail.com>
2021-12-13 12:49:55 +0100

    Day13 solved.

--- 2021/Day13.java
+++ 2021/Day13.java
@@ -27,15 +27,77 @@ public class Day13 {
             }
         }
 
+        // Set grid
+        int xMax = 0; int yMax = 0;
+        for (int[] coord : coords) {
+            if (coord[0] > xMax) {
+                xMax = coord[0];
+            }
+            if (coord[1] > yMax) {
+                yMax = coord[1];
+            }
+            
+        }
+        int[][] grid = new int[yMax+1][xMax+1];
+        for (int[] co : coords) {
+            grid[co[1]][co[0]] = 1;
+        }
 
-        // System.out.format("Part 1: %d\n", );
+        // Fold
+        int foldCount = 0;
+        for (String[] fold: folds) {
+            String axis = fold[0];
+            int foldPoint = Integer.parseInt(fold[1]);
+            if (axis.equals("x")) {
+                for (int i = 0; i <= yMax; i++) {                                    
+                    for (int j = foldPoint+1; j <= xMax; j++) {
+                        if (grid[i][j] == 1) {
+                            int diff = j - foldPoint;
+                            grid[i][foldPoint - diff] = 1;
+                            grid[i][j] = 0;
+                        }
+                    }
+                }
+                xMax = xMax/2;
+            } else {
+                for (int i = 0; i <= xMax; i++) {
+                    for (int j = foldPoint+1; j <= yMax; j++) {
+                        if (grid[j][i] == 1) {
+                            int diff = j - foldPoint;
+                            grid[foldPoint - diff][i] = 1;
+                            grid[j][i] = 0;
+                        }
+                    }
+                }
+                yMax = yMax/2;
+            }
+
+            foldCount++;
+            int sum = 0;
+            for (int[] is : grid) {
+                sum += Arrays.stream(is).reduce(0, (a, b) -> a + b);
+            }
+
+            // Print solution part 1
+            if (foldCount == 1) {
+                System.out.format("Part 1: %d\n", sum);
+            }
+        }
+
+        // Print final grid
+        for (int i = 0; i < yMax; i++) {
+            for (int j = 0; j < xMax; j++) {
+                if (grid[i][j] == 0) {
+                    System.out.printf(".");
+                } else {
+                    System.out.printf("#");
+                }
+            }
+            System.out.println("");
+        }
     }
 
-    public static void solveTwo(String filename) {
-        // System.out.format("Part 2: %d\n", );
-    }
     public static void main(String[] args) {
         solveOne(args[0]);
-        // Part 2, 27994957 too high
     }
 }
