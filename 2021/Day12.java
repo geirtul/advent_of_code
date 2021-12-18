41aa150426aeee0614a3f3f5205e1d0eacc5dfd3
Geir Ulvik <gt.ulvik@gmail.com>
2021-12-18 01:19:46 +0100

    Adding template for day 12

---
+++ 2021/Day12.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Day12 {
+
+    public static void solveOne(String filename) {
+        // System.out.printf("Part 1: %d\n", );
+    }
+
+    public static void main(String[] args) {
+        solveOne(args[0]);
+    }
+}
