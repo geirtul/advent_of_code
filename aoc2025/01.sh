5506c357e111b207b96b4c8481d43fb5b5760d78
Geir Ulvik <gt.ulvik@gmail.com>
2025-12-01 16:52:02 +0100

    Update 01.sh

--- aoc2025/01.sh
+++ aoc2025/01.sh
@@ -5,23 +5,30 @@ mapfile -t lines < "$filename"
 
 curr=50
 regex="([[:alpha:]])([[:digit:]]+)"
-rotate() {
-    if [[ "$1" =~ $regex ]]; then
+
+num_zeros=0
+for move in "${lines[@]}"; do
+    if [[ "$move" =~ $regex ]]; then
         dir="${BASH_REMATCH[1]}"
-        num=$(( BASH_REMATCH[2] % 100 ))
+        num="${BASH_REMATCH[2]}"
+        prev=$curr
+        full_rotations=$(( num / 100 ))
+        (( num % 100 ))
 
         case $dir in
             L) (( curr -= num )) ;;
             R) (( curr += num )) ;;
         esac
 
-        (( curr = (curr + 100) % 100 ))
-    fi
-}
+        if (( prev != 0 )); then
+            (( num_zeros += curr < 0 || curr > 100 ? 1 : 0))
+        fi
 
-num_zeros=0
-for move in "${lines[@]}"; do
-    rotate "$move"
-    (( num_zeros += curr == 0 ? 1 : 0))
+        (( curr = (curr + 100) % 100 ))
+        (( num_zeros += curr == 0 ? 1 : 0 ))
+
+        # echo "$move: $num_zeros, full: $full_rotations, prev: $prev, curr: $curr"
+    fi
+
 done
 echo "The code is: $num_zeros"
