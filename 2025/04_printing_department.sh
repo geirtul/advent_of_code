8833e9efcc8fa5d0705a2c9d77543cbeaf4d1e72
Geir Ulvik <gt.ulvik@gmail.com>
2025-12-11 23:25:51 +0100

    day 4 complete. uggo but works

--- 2025/04_printing_department.sh
+++ 2025/04_printing_department.sh
@@ -24,38 +24,38 @@ for (( row=0; row < num_rows; row++ )); do
 	
 	count=0
 	if (( row == 0 )); then
-	    [[ "${lines[row]:col-1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row]:col+1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row+1]:col-1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row+1]:col:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row+1]:col+1:1}" == "@" ]] && (( count++ ))
+	    [[ "${lines[row]:col-1:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row]:col+1:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row+1]:col-1:1}"    == "@" ]] && (( count++ ))
+	    [[ "${lines[row+1]:col:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row+1]:col+1:1}"    == "@" ]] && (( count++ ))
 	elif (( row == num_rows - 1 )); then
-	    [[ "${lines[row]:col-1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row]:col+1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row-1]:col-1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row-1]:col:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row-1]:col+1:1}" == "@" ]] && (( count++ ))
+	    [[ "${lines[row]:col-1:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row]:col+1:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row-1]:col-1:1}"    == "@" ]] && (( count++ ))
+	    [[ "${lines[row-1]:col:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row-1]:col+1:1}"    == "@" ]] && (( count++ ))
 	elif (( col == 0 )); then
-	    [[ "${lines[row-1]:col:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row+1]:col:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row-1]:col+1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row]:col+1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row+1]:col+1:1}" == "@" ]] && (( count++ ))
+	    [[ "${lines[row-1]:col:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row+1]:col:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row-1]:col+1:1}"    == "@" ]] && (( count++ ))
+	    [[ "${lines[row]:col+1:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row+1]:col+1:1}"    == "@" ]] && (( count++ ))
 	elif (( col == num_cols - 1 )); then
-	    [[ "${lines[row-1]:col:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row+1]:col:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row-1]:col-1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row]:col-1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row+1]:col-1:1}" == "@" ]] && (( count++ ))
+	    [[ "${lines[row-1]:col:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row+1]:col:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row-1]:col-1:1}"    == "@" ]] && (( count++ ))
+	    [[ "${lines[row]:col-1:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row+1]:col-1:1}"    == "@" ]] && (( count++ ))
 	else
-	    [[ "${lines[row-1]:col-1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row]:col-1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row+1]:col-1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row-1]:col:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row+1]:col:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row-1]:col+1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row]:col+1:1}" == "@" ]] && (( count++ ))
-	    [[ "${lines[row+1]:col+1:1}" == "@" ]] && (( count++ ))
+	    [[ "${lines[row-1]:col-1:1}"    == "@" ]] && (( count++ ))
+	    [[ "${lines[row]:col-1:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row+1]:col-1:1}"    == "@" ]] && (( count++ ))
+	    [[ "${lines[row-1]:col:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row+1]:col:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row-1]:col+1:1}"    == "@" ]] && (( count++ ))
+	    [[ "${lines[row]:col+1:1}"      == "@" ]] && (( count++ ))
+	    [[ "${lines[row+1]:col+1:1}"    == "@" ]] && (( count++ ))
 	fi
 	
 	(( count < 4 )) && (( accessible++ ))
@@ -65,4 +65,78 @@ done
 
 echo "Part 1: $accessible"
 
-echo "Part 2: "
+accessible=0
+roll_indices=()
+
+while :; do
+    for (( row=0; row < num_rows; row++ )); do
+        for (( col=0; col < num_cols; col++ )); do
+
+        if [[ "${lines[row]:col:1}" != "@" ]]; then
+            continue
+        fi
+
+        # Corners
+        if (( (row == 0 || row == num_rows - 1) &&
+            (col == 0 || col == num_cols - 1) )); then
+            (( accessible++ ))
+            roll_indices+=("$row $col")
+            continue
+        fi
+        
+        count=0
+        if (( row == 0 )); then
+            [[ "${lines[row]:col-1:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row]:col+1:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row+1]:col-1:1}"    == "@" ]] && (( count++ ))
+            [[ "${lines[row+1]:col:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row+1]:col+1:1}"    == "@" ]] && (( count++ ))
+        elif (( row == num_rows - 1 )); then
+            [[ "${lines[row]:col-1:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row]:col+1:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row-1]:col-1:1}"    == "@" ]] && (( count++ ))
+            [[ "${lines[row-1]:col:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row-1]:col+1:1}"    == "@" ]] && (( count++ ))
+        elif (( col == 0 )); then
+            [[ "${lines[row-1]:col:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row+1]:col:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row-1]:col+1:1}"    == "@" ]] && (( count++ ))
+            [[ "${lines[row]:col+1:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row+1]:col+1:1}"    == "@" ]] && (( count++ ))
+        elif (( col == num_cols - 1 )); then
+            [[ "${lines[row-1]:col:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row+1]:col:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row-1]:col-1:1}"    == "@" ]] && (( count++ ))
+            [[ "${lines[row]:col-1:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row+1]:col-1:1}"    == "@" ]] && (( count++ ))
+        else
+            [[ "${lines[row-1]:col-1:1}"    == "@" ]] && (( count++ ))
+            [[ "${lines[row]:col-1:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row+1]:col-1:1}"    == "@" ]] && (( count++ ))
+            [[ "${lines[row-1]:col:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row+1]:col:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row-1]:col+1:1}"    == "@" ]] && (( count++ ))
+            [[ "${lines[row]:col+1:1}"      == "@" ]] && (( count++ ))
+            [[ "${lines[row+1]:col+1:1}"    == "@" ]] && (( count++ ))
+        fi
+        
+        if (( count < 4 )); then
+            (( accessible++ ))
+            roll_indices+=("$row $col")
+        fi
+
+        done
+    done
+
+    (( ${#roll_indices} > 0 )) || break
+
+    for indices in "${roll_indices[@]}"; do
+        read -r row col <<< "$indices"
+        line="${lines[row]}"
+        lines[row]="${line:0:col}x${line:col+1}"
+    done
+
+    roll_indices=()
+done
+
+echo "Part 2: $accessible"
